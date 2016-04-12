36aafdd70436641ff9c679ab70326e8bd67c4d2e
Jesper Lindström <raxezdev@gmail.com>
2016-04-12 20:56:50 +0200

    Added foundation via bower

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 # digIT design
 Optional design library for building coherently looking digIT web services.
 
+## Introduction
+The idea with this repository is to provide an extensible set of digIT branded base components, with some example layouts and templates. The goal is to create a simple unified look to make development of new services easier and make digIT services feel more coherent.
+
 ## Install
-Requires npm and gulp.
+Requires npm, bower and gulp.
 
 ```
 npm install
+bower install
 ```
 
 ## Run local documentation development server
--- gulpfile.js
+++ gulpfile.js
@@ -5,25 +5,32 @@ var browserSync = require('browser-sync').create();
 var cleanCSS = require('gulp-clean-css');
 var rename = require('gulp-rename');
 
+var paths = {
+  'scss': [
+    'bower_components/foundation-sites/scss'
+  ]
+}
+
 gulp.task('clean', function() {
   return gulp.src('dist').pipe(clean());
 });
 
 gulp.task('release', ['clean'], function() {
-  gulp.src('src/scss/style.scss')
-    .pipe(sass().on('error', sass.logError))
+  gulp.src('src/scss/app.scss')
+    .pipe(sass({ includePaths: paths.scss }).on('error', sass.logError))
     .pipe(cleanCSS())
     .pipe(rename('digit.min.css'))
     .pipe(gulp.dest('./dist/'));
 });
 
 gulp.task('build', ['clean'], function() {
-  gulp.src('src/scss/style.scss')
-    .pipe(sass().on('error', sass.logError))
+  gulp.src('src/scss/app.scss')
+    .pipe(sass({ includePaths: paths.scss }).on('error', sass.logError))
     .pipe(gulp.dest('./build/css/'))
+    .pipe(browserSync.stream());
 
   gulp.src('./src/**/*.html')
-    .pipe(gulp.dest('./build/'))
+    .pipe(gulp.dest('./build/'));
 });
 
 gulp.task('reload', ['build'], browserSync.reload);
